3c9290a5d38f33b2986b391b006742dfaef78656
siyoung-yoo <lab.910@outlook.com>
2025-12-05 17:49:54 +0900

    work14 작업본 정리중

--- resource/work14/adiga/static/js/comm/pub_ui.js
+++ resource/work14/adiga/static/js/comm/pub_ui.js
@@ -20,3 +20,49 @@ function initLayoutInclude() {
         });
     });
 }
+
+function fnOnEventToggle(selector){
+	if(!$(selector).children('.modPopup').hasClass("on")){
+        $("html").addClass('scrollFix')
+		$(selector).children('.modPopup').addClass("on");
+	} else {
+        $("html").removeClass('scrollFix')
+		$(selector).children('.modPopup').removeClass("on");
+	}
+}
+
+$(document).on("click", "#btn0201", function() {
+    $("#popup02 .state").addClass("hide");
+    $("#popup02 .state._ing").removeClass("hide");
+    $("#btn0201").addClass("hide")
+    $("#btn0202").removeClass("hide")
+
+    setTimeout(function () {
+        $("#popup02 .state").addClass("hide");
+        $("#popup02 .state._done").removeClass("hide");
+        $("#btn0202").addClass("hide")
+        $("#btn0203").removeClass("hide")
+    }, 1000)
+})
+
+$(document).on("click", ".btn0301", function () {
+    if ($(this).prev('input').val() === '1234') {
+        const list = $(this).closest("li");
+        list.find(".examNumArea").removeClass("on");
+        list.find(".depTmt").addClass("on");
+        list.find(".univAdm.null").addClass("hide");
+        list.find(".univAdm.value").removeClass("hide");
+        list.find(".tooltipCont").removeClass("hide");
+    } else {
+        alert("수험 번호가 일치하지 않습니다.\n수험번호는 '1234' 입니다.")
+    }
+})
+
+function printPopup() {
+    document.body.classList.add('printOnlyPopup')
+    window.print();
+}
+
+window.addEventListener("afterprint", () =>
+    document.body.classList.remove('printOnlyPopup')
+);
